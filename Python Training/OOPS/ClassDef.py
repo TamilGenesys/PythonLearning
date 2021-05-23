d19cbf9eed7aefe258782d88611c5d3167bd57f8
TamilGenesys <tamil.genesys@gmail.com>
2021-05-23 15:10:37 +0530

    New Begining

---
+++ Python Training/OOPS/ClassDef.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 23 14:57:57 2021
+
+@author: tamil
+"""
+
+
+class Program:
+    def __init__(self, name):
+        self.name = name
+
+java = Program("Java Language")
+print(java)
+print(java.name)
